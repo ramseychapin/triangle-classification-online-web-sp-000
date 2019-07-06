60e4bb6773c33fab42494240db17056e536cd1b2
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-06 23:05:48 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :all_sides
+
+  def initialize(side1, side2, side3)
+    @all_sides = []
+    @all_sides << side1
+    @all_sides << side2
+    @all_sides << side3
+    @side_match_counter = 0
+  end
+
+  def positive?
+    self.all_sides.all? {|side| side > 0}
+  end
+
+  def is_triangle?
+    sorted_triangle = self.all_sides.sort
+    biggest_side = sorted_triangle.pop
+    biggest_side < sorted_triangle.sum
+  end
+
+  def triangle_type
+    @side_match_counter = self.all_sides.uniq.size
+  end
+
+
+  def kind
+    if(positive? == false || is_triangle? == false)
+        raise TriangleError
+    else
+      triangle_type
+      if (@side_match_counter == 1)
+        :equilateral
+      elsif (@side_match_counter == 2)
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "not a triangle!!"
+    end
+  end
+
+
 end
