a7cd26655b7f9a47685ae971be7481ea90e123d8
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-06 22:46:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3, :all_sides
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @all_sides = []
+    @all_sides << @side1
+    @all_sides << @side2
+    @all_sides << @side3
+  end
+
+  def positive?
+    self.all_sides.all? {|side| side > 0}
+  end
+
+  def is_triangle?
+    sorted_triangle = self.all_sides.sort { |a,b| a <=> b }
+    biggest_side = sorted_triangle.pop
+    biggest_side > sorted_triangle.sum
+  end
+
+  def kind
+    if(positive? == false || is_triangle? == false)
+        raise TriangleError
+    elsif self.all_sides.all? { |side| side == @all_sides[0]}
+      :equilateral
+    else
+      puts "nope!"
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "not a triangle!!"
+    end
+  end
+
+
 end
